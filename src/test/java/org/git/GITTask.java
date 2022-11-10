1c5b9c7fcc7ee35ebba1e389482e3d681893d922
Akshayaa1612 <akshayaamurugan21196@gmail.com>
2022-11-10 13:45:38 +0530

    akshayaacode

---
+++ src/test/java/org/git/GITTask.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class GITTask {
+
+	public static void main(String[] args) {
+		
+	}
+	private void chennai() {
+System.out.println("MAS");
+	}
+	private void bangalore() {
+System.out.println("KBS");
+	}
+	private void coimbatore() {
+System.out.println("CBE");
+	}
+}
